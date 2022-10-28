c08b013c22dfc359b3df8c07996be09aa22e4210
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2022-10-28 17:31:37 +0000

    Lista de clientes

--- src/front/js/component/listaAlumnos.js
+++ src/front/js/component/listaAlumnos.js
@@ -1,92 +1,99 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
-export const ListaAlumno = ()  => {
-    return (
-        <>
-        <div className="container">
+export const ListaAlumno = () => {
+  const { store, actions } = useContext(Context);
 
+  return (
+    <>
+      <div className="container">
         <div>
-            <label htmlFor="exampleDataList" className="form-label"><h3>Buscar</h3></label>
-                <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Buscar..."/>
-                    <datalist id="datalistOptions"/>
-                    <option value="San Francisco"/>
-                    <option value="New York"/>
-                    <option value="Seattle"/>
-                    <option value="Los Angeles"/>
-                    <option value="Chicago"/>
-        </div>  
+          <br />
+          <h1>Alumnos</h1>
+        </div>
+
+        {/* Buscar */}
+        <div>
+          <label htmlFor="exampleDataList" className="form-label">
+            <h3>Buscar</h3>
+          </label>
+          <input
+            className="form-control"
+            list="datalistOptions"
+            id="exampleDataList"
+            placeholder="Buscar..."
+          />
+        </div>
+
+        {/* Nuevo Alumno */}
+        <br />
         <div className="w-75">
           <Link to={"/crearAlumno"}>
-              <button type="button" className="btn btn-outline-success w-25">Nuevo Alumno</button>
+            <button type="button" className="btn btn-outline-success w-25">
+              Ingresar nuevo alumno
+            </button>
           </Link>
         </div>
+        <br />
+
+        {/* Listado de alumnos */}
         <table className="table table-hover table-secondary">
-  <thead>
-    <tr>
-      <th scope="col">Cedula</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Apellido</th>
-      <th scope="col">Fecha de nacimiento</th>
-      <th scope="col">Fecha de ingreso</th>
-      <th scope="col">Mutualista</th>
-      <th scope="col">Afecciones</th>
-      <th scope="col">Medicamentos</th>
-      <th scope="col">Email</th>
-      <th scope="col">Actividades</th>
-      <th scope="col">Ver Rutina</th>
-      <th scope="col">Editar y borrar</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <button type="button" className="btn btn-outline-dark">Editar</button>
-      <button type="button" className="btn btn-outline-danger">Borrar</button>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <button type="button" className="btn btn-outline-dark">Editar</button>
-      <button type="button" className="btn btn-outline-danger">Borrar</button>
-    </tr>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <button type="button" className="btn btn-outline-dark">Editar</button>
-      <button type="button" className="btn btn-outline-danger">Borrar</button>
-    </tr>
-  </tbody>
-</table>
-        </div>
-        </>
-    );
+          <thead>
+            <tr>
+              {/* Cabezeras */}
+              <th scope="col">Cedula</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellido</th>
+              <th scope="col">Fecha de nacimiento</th>
+              <th scope="col">Fecha de ingreso</th>
+              <th scope="col">Mutualista</th>
+              <th scope="col">Afecciones</th>
+              <th scope="col">Medicamentos</th>
+              <th scope="col">Email</th>
+              <th scope="col">Actividades</th>
+              <th scope="col">Rutina</th>
+              <th scope="col">Editar</th>
+              <th scope="col">Borrar</th>
+            </tr>
+          </thead>
+
+          {/* Listado */}
+          {/* <thead> */}
+          <tbody>
+            {store.alumnos.map((item, id) => (
+              <tr key={id}>
+                <td>{item.ci}</td>
+                <td>{item.name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.birthday}</td>
+                <td>{item.date_of_admission}</td>
+                <td>{item.mutualist}</td>
+                <td>{item.medical_conditions}</td>
+                <td>{item.medicines}</td>
+                <td>{item.email}</td>
+                <td>{item.activities}</td>
+                <td>
+                  <button type="button" className="btn btn-outline-dark">
+                    <i class="fa fa-eye"></i>
+                  </button>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-outline-dark">
+                    <i class="fa fa-pen"></i>
+                  </button>
+                </td>
+
+                <td>
+                  <button type="button" className="btn btn-outline-danger">
+                    <i class="fa fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
