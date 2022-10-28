361d7f6298640c46adc12c85f9b2a6c4d1608861
juanvoo <juanmoreiracosta2016@gmail.com>
2022-10-28 20:52:51 +0000

    botones planes y quienes somos

---
+++ src/front/js/component/quienesSomos.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+export const QuienesSomos = ()  => {
+    return (
+        <>
+        <div className="container">
+        <div><h1>QUIENES SOMOS!</h1></div>
+        <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Misión</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+        </div>
+        
+        </>
+    )
+}
