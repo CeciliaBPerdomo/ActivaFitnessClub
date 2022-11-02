65b58839eadee250b956a16eab3db49fdc7c42eb
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2022-11-02 15:24:42 +0000

    Correcion de rutas

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 // import json;
 
+let direccion =
+    "https://3001-ceciliabper-activafitne-eli31wbrf68.ws-us73.gitpod.io";
+
 const getState = ({
     getStore,
     getActions,
@@ -38,25 +41,23 @@ const getState = ({
                 is_active
             ) => {
                 try {
-                    const response = await axios.post(
-                        "https://3001-ceciliabper-activafitne-eli31wbrf68.ws-us73.gitpod.io/api/user", {
-                            ci: ci,
-                            name: name,
-                            last_name: last_name,
-                            phone: phone,
-                            date_of_admission: date_of_admission,
-                            birthday: birthday,
-                            mutualist: mutualist,
-                            medical_conditions: medical_conditions,
-                            medicines: medicines,
-                            training_goals: training_goals,
-                            email: email,
-                            password: password,
-                            activities: activities,
-                            role: role,
-                            is_active: is_active,
-                        }
-                    );
+                    const response = await axios.post(direccion + "/api/user", {
+                        ci: ci,
+                        name: name,
+                        last_name: last_name,
+                        phone: phone,
+                        date_of_admission: date_of_admission,
+                        birthday: birthday,
+                        mutualist: mutualist,
+                        medical_conditions: medical_conditions,
+                        medicines: medicines,
+                        training_goals: training_goals,
+                        email: email,
+                        password: password,
+                        activities: activities,
+                        role: role,
+                        is_active: is_active,
+                    });
                     return true;
                 } catch (error) {
                     console.log(error);
@@ -66,9 +67,7 @@ const getState = ({
             /* Listar alumnos */
             obtenerAlumnos: async () => {
                 try {
-                    const response = await axios.get(
-                        "https://3001-ceciliabper-activafitne-eli31wbrf68.ws-us73.gitpod.io/api/user", {}
-                    );
+                    const response = await axios.get(direccion + "/api/user", {});
                     //console.log(response.data);
                     setStore({
                         alumnos: response.data,
@@ -85,8 +84,7 @@ const getState = ({
             borrarAlumno: async (id) => {
                 try {
                     const response = await axios.delete(
-                        "https://3001-ceciliabper-activafitne-nmi4dt8tju0.ws-us73.gitpod.io/api/user/" +
-                        id, {}
+                        direccion + "/api/user/" + id, {}
                     );
                     // console.log(response);
                 } catch (error) {
@@ -99,10 +97,7 @@ const getState = ({
 
             obtenerAlumnoId: async (id) => {
                 try {
-                    const response = await axios.get(
-                        "https://3001-ceciliabper-activafitne-nmi4dt8tju0.ws-us73.gitpod.io/api/user/" +
-                        id, {}
-                    );
+                    const response = await axios.get(direccion + "/api/user/" + id, {});
                     // console.log(response.data);
                     setStore({
                         alumno: response.data,
@@ -134,26 +129,23 @@ const getState = ({
                 isActive
             ) => {
                 try {
-                    const response = await axios.put(
-                        "https://3001-ceciliabper-activafitne-nmi4dt8tju0.ws-us73.gitpod.io/api/user/" +
-                        id, {
-                            ci: ci,
-                            name: name,
-                            last_name: last_name,
-                            phone: phone,
-                            date_of_admission: admission,
-                            birthday: birthday,
-                            mutualist: mutualist,
-                            medical_conditions: conditions,
-                            medicines: medicines,
-                            training_goals: training_goals,
-                            email: mail,
-                            password: password,
-                            activities: activities,
-                            role: role,
-                            is_active: isActive,
-                        }
-                    );
+                    const response = await axios.put(direccion + "/api/user/" + id, {
+                        ci: ci,
+                        name: name,
+                        last_name: last_name,
+                        phone: phone,
+                        date_of_admission: admission,
+                        birthday: birthday,
+                        mutualist: mutualist,
+                        medical_conditions: conditions,
+                        medicines: medicines,
+                        training_goals: training_goals,
+                        email: mail,
+                        password: password,
+                        activities: activities,
+                        role: role,
+                        is_active: isActive,
+                    });
                     console.log(response.data);
                 } catch (error) {
                     console.log(error);
@@ -171,8 +163,7 @@ const getState = ({
             obtenerMensualidadId: async (id) => {
                 try {
                     const response = await axios.get(
-                        "https://3001-ceciliabper-activafitne-eli31wbrf68.ws-us73.gitpod.io/api/mensualidad/" +
-                        id, {}
+                        direccion + "/api/mensualidad/" + id, {}
                     );
                     console.log(response.data);
                     setStore({
@@ -188,7 +179,7 @@ const getState = ({
             obtenerMensualidades: async () => {
                 try {
                     const response = await axios.get(
-                        "https://3001-ceciliabper-activafitne-eli31wbrf68.ws-us73.gitpod.io/api/mensualidades", {}
+                        direccion + "/api/mensualidades", {}
                     );
                     //console.log(response.data);
                     setStore({
@@ -212,8 +203,7 @@ const getState = ({
             ) => {
                 try {
                     const response = await axios.put(
-                        "https://3001-ceciliabper-activafitne-342eyp2q71g.ws-us73.gitpod.io//api/mensualidades/" +
-                        id, {
+                        direccion + "/api/mensualidades/" + id, {
                             id: id,
                             user_id: user_id,
                             payment_date: payment_date,
