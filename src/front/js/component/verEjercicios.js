943d23391ab270d9366fe28ed032fca6398c2906
federicaramirez <federicaaramirez@gmail.com>
2022-11-07 17:44:11 +0000

    arreglando el estilo a vista ejercicio

--- src/front/js/component/verEjercicios.js
+++ src/front/js/component/verEjercicios.js
@@ -10,61 +10,61 @@ export const VerEjercicio = () => {
         actions.obtenerEjercicios();
     }, []);
 
-    return (
-        <> {/* Buscar */}
-            <div>
-                <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Buscar por nombre..."/>
-            </div>
+    return (<> {/* Buscar */}
+        <div>
+            <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Buscar por nombre..."/>
+        </div>
 
-            <div className="d-flex">
-                {
-                store.ejercicios.map((item, id) => (
-                    <div className="card"
+        <div className="d-flex"> {
+            store.ejercicios.map((item, id) => (<div className="card"
+                style={
+                    {
+                        width: "25%",
+                        margin: "20px",
+                        borderRadius: "6px",
+                        overflow: "hidden",
+                        background: "#ffff",
+                        boxShadow: "0px 1px 10px rgb(0, 0, 0, 0.2)",
+                        cursor: "default",
+                        transition: "all 400ms ease"
+                    }
+            }>
+
+
+                <div className="card-body">
+                    <h2 className="display-6"
+                        style={
+                            {marginBottom: "15px"}
+                    }>
+                        <i className="fa fa-dumbbell"></i>
+                        {
+                        item.exercise_name
+                    }</h2>
+                    <img src={
+                            item.photo_exercise
+                        }
                         style={
                             {
-                                width: "25%",
-                                margin: "20px",
-                                borderRadius: "6px",
-                                overflow: "hidden",
-                                background: "#ffff",
-                                boxShadow: "0px 1px 10px rgb(0, 0, 0, 0.2)",
-                                cursor: "default",
-                                transition: "all 400ms ease"
+                                width: "100%",
+                                height: "210px"
                             }
+                        }
+                        className="card-img-top"
+                        alt="..."/>
+
+                </div>
+                <div className="card-body text-center">
+
+                    <Link type="button" className="btn btn-outline-info"
+                        to={
+                            "/vistaEjercicio/" + item.id
                     }>
+                        <i className="fa fa-eye"></i>
+                    </Link>
 
+                </div>
+            </div>))
+        } </div>
 
-                        <div className="card-body">
-                            <h2 className="display-6"
-                                style={
-                                    {marginBottom: "15px"}
-                            }>
-                                <i className="fa fa-dumbbell"></i>
-                                {
-                                item.exercise_name
-                            }</h2>
-                            <img src={
-                                    item.photo_exercise
-                                }
-                                style={
-                                    {
-                                        width: "100%",
-                                        height: "210px"
-                                    }
-                                }
-                                className="card-img-top"
-                                alt="..."/>
-
-                        </div>
-                        <div className="card-body text-center">
-                            <button type="button" className="btn btn-outline-info">
-                                <i className="fa fa-eye"></i>
-                            </button>
-                        </div>
-                    </div>
-                ))
-            } </div>
-
-        </>
-    )
+    </>)
 }
--- src/front/js/component/vistaEjercicio.jsx
+++ src/front/js/component/vistaEjercicio.jsx
@@ -26,70 +26,179 @@ export const VistaEjercicio = () => {
     }, [params.theid]);
 
 
-    return (
-        <>
-            <br/> {/* Titulo */}
-            <h1 className="justify-content-center d-flex">
-                <i className="fa fa-dumbbell"></i>
+    return (<>
+
+        <br/> {/* Titulo */}
+        <h1 className="justify-content-center d-flex">
+            <i className="fa fa-dumbbell"></i>
+            {
+            store.ejercicio.exercise_name
+        } </h1>
+        <br/>
+
+
+        <div className="justify-content-center d-flex"
+            style={
                 {
-                store.ejercicio.exercise_name
-            } </h1>
-            <br/>
-            <div className="justify-content-center d-flex">
-                <div className="card"
-                    style={
-                        {width: "40rem"}
+
+                    width: "50%",
+                    display: "flex",
+                    margin: "auto",
+                    borderRadius: "7px",
+                    boxShadow: "0px 14px 32px 0px rgb(0, 0, 0, 0.5)",
+                    overflow: "hidden",
+                    boxSizing: "border-box"
+                }
+
+        }>
+            <div className="card"
+                style={
+                    {
+                        width: "40rem",
+                        display: "flex",
+                        margin: "auto",
+                        borderRadius: "7px"
+                    }
+            }>
+                <div style={
+                    {borderRadius: "10 5 5 5 5"}
                 }>
                     <img src={
                             store.ejercicio.photo_exercise
                         }
                         className="card-img-top"
                         alt="..."/>
-                    <div className="card-body">
-                        <p className="card-text">
-                            {
-                            store.ejercicio.description
-                        }</p>
-                    </div>
-                    <ul className="list-group list-group-flush">
-
-                        <div className="d-flex">
-                            <ReactPlayer // style={
-                                //         {
-                                //             width: "370",
-                                //             height: "215"
-                                //         }
-                                //     }
-                                url={
-                                    store.ejercicio.video_exercise
-                                }
-                                controls
-                                playing
-                                muted></ReactPlayer>
-                        </div>
-
-                    </ul>
-
                 </div>
-            </div>
+                <div className="card-body"
+                    style={
+                        {width: "50%"}
+                }>
+                    <p className="card-text"> {
+                        store.ejercicio.description
+                    }</p>
+                </div>
+                <ul className="list-group list-group-flush">
 
-            <br/>
-            <div className="d-flex justify-content-end " role="group" aria-label="Basic outlined example"
-                style={
-                    {marginRight: "70px"}
-            }>
-                <Link to={
-                    "/vistaEjercicio/" + down
-                }>
-                    <button className="btn btn-outline-primary">Down</button>
-                </Link>
-                <Link to={
-                    "/vistaEjercicio/" + next
-                }>
-                    <button className="btn btn-outline-primary">Next</button>
-                </Link>
+                    <div className="d-flex">
+                        <ReactPlayer // style={
+                            //         {
+                            //             width: "370",
+                            //             height: "215"
+                            //         }
+                            //     }
+                            url={
+                                store.ejercicio.video_exercise
+                            }
+                            controls
+                            playing
+                            muted></ReactPlayer>
+                    </div>
+
+                </ul>
+
             </div>
-            <br/>
-        </>
-    )
+        </div>
+
+        <br/>
+        <div className="d-flex justify-content-end " role="group" aria-label="Basic outlined example"
+            style={
+                {marginRight: "70px"}
+        }>
+            <Link to={
+                "/vistaEjercicio/" + down
+            }>
+                <button className="btn btn-outline-primary">Down</button>
+            </Link>
+            <Link to={
+                "/vistaEjercicio/" + next
+            }>
+                <button className="btn btn-outline-primary">Next</button>
+            </Link>
+        </div>
+        <br/>
+        */
+
+    </>)
 };
+
+
+{ /* <div className="justify-content-center d-flex"
+style={
+    {
+
+        width: "50%",
+        display: "flex",
+        margin: "auto",
+        borderRadius: "7px",
+        boxShadow: "0px 14px 32px 0px rgb(0, 0, 0, 0.5)",
+        overflow: "hidden",
+        boxSizing: "border-box"
+    }
+
+}>
+<div className="card"
+    style={
+        {
+            width: "40rem",
+            display: "flex",
+            margin: "auto",
+            borderRadius: "7px"
+        }
+}>
+    <div style={
+        {borderRadius: "10 5 5 5 5"}
+    }>
+        <img src={
+                store.ejercicio.photo_exercise
+            }
+            className="card-img-top"
+            alt="..."/>
+    </div>
+    <div className="card-body"
+        style={
+            {width: "50%"}
+    }>
+        <p className="card-text"> {
+            store.ejercicio.description
+        }</p>
+    </div>
+    <ul className="list-group list-group-flush">
+
+        <div className="d-flex">
+            <ReactPlayer // style={
+                //         {
+                //             width: "370",
+                //             height: "215"
+                //         }
+                //     }
+                url={
+                    store.ejercicio.video_exercise
+                }
+                controls
+                playing
+                muted></ReactPlayer>
+        </div>
+
+    </ul>
+
+</div>
+</div>
+
+<br/>
+<div className="d-flex justify-content-end " role="group" aria-label="Basic outlined example"
+style={
+    {marginRight: "70px"}
+}>
+<Link to={
+    "/vistaEjercicio/" + down
+}>
+    <button className="btn btn-outline-primary">Down</button>
+</Link>
+<Link to={
+    "/vistaEjercicio/" + next
+}>
+    <button className="btn btn-outline-primary">Next</button>
+</Link>
+</div>
+<br/> */
+}
