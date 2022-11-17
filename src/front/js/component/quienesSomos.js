21927bfe9e6f14a28311476f80dd498255d70a0a
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2022-11-17 13:32:04 +0000

    Perfiles

--- src/front/js/component/quienesSomos.js
+++ src/front/js/component/quienesSomos.js
@@ -22,156 +22,189 @@ export const QuienesSomos = () => {
         <div className="row">
           {/* Ceci */}
           <div className="col" style={{ margin: "10px" }}>
-            <div className="card" style={{ width: "18rem" }}>
-              <img
-                src={ceci}
-                className="card-img-top rounded-circle"
-                style={{ margin: "5px", width: "97%" }}
-              />
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <h2
-                    style={{
-                      color: "blueviolet",
-                      "font-family": "Inspiration, cursive",
-                      "font-size": "48px",
-                      marginTop: "20px",
-                    }}
-                  >
-                    Cecilia Perdomo
-                  </h2>
-                </li>
-                <li className="list-group-item">
-                  <p>Full stack developer</p>
-                  Colonia del Sacramento
-                </li>
-                <li className="list-group-item">
+            <div className="card mb-3" style={{ width: "540px" }}>
+              <div className="row g-0">
+                <div className="col-md-4">
                   <img
-                    src={lk}
-                    className="card-img-top rounded-circle float-end"
-                    style={{ margin: "5px", width: "20%" }}
-                    onClick={() =>
-                      (window.location.href =
-                        "https://www.linkedin.com/in/cecilia-perdomo/")
-                    }
+                    src={ceci}
+                    className="img-fluid rounded-circle"
+                    style={{ margin: "10px", width: "97%", marginTop: "20px" }}
                   />
-                </li>
-              </ul>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5
+                      className="card-title"
+                      style={{
+                        color: "blueviolet",
+                        "font-family": "Inspiration, cursive",
+                        "font-size": "48px",
+                        marginTop: "15px",
+                      }}
+                    >
+                      Cecilia Perdomo
+                    </h5>
+                    <p className="card-text">
+                      Full Stack developer <br />
+                      Colonia del Sacramento, Colonia
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        <img
+                          src={lk}
+                          className="card-img-top rounded-circle float-end"
+                          style={{ margin: "10px", width: "12%" }}
+                          onClick={() =>
+                            (window.location.href =
+                              "https://www.linkedin.com/in/cecilia-perdomo/")
+                          }
+                        />
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           {/* Fede */}
           <div className="col" style={{ margin: "10px" }}>
-            <div className="card" style={{ width: "18rem" }}>
-              <img
-                src={fede}
-                className="card-img-top rounded-circle"
-                style={{ margin: "5px", width: "97%" }}
-              />
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <h2
-                    style={{
-                      color: "blueviolet",
-                      "font-family": "Inspiration, cursive",
-                      "font-size": "48px",
-                      marginTop: "20px",
-                    }}
-                  >
-                    Federica Ramirez
-                  </h2>
-                </li>
-                <li className="list-group-item">
-                  <p>Full stack developer</p>
-                  Tacuarembo
-                </li>
-                <li className="list-group-item">
+            <div className="card mb-3" style={{ width: "540px" }}>
+              <div className="row g-0">
+                <div className="col-md-4">
                   <img
-                    src={lk}
-                    className="card-img-top rounded-circle float-end"
-                    style={{ margin: "5px", width: "20%" }}
-                    onClick={() =>
-                      (window.location.href =
-                        "https://www.linkedin.com/in/federica-ram%C3%ADrez-peluffo-b23bb0197/")
-                    }
+                    src={fede}
+                    className="img-fluid rounded-circle"
+                    style={{ margin: "10px", width: "97%", marginTop: "20px" }}
                   />
-                </li>
-              </ul>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5
+                      className="card-title"
+                      style={{
+                        color: "blueviolet",
+                        "font-family": "Inspiration, cursive",
+                        "font-size": "48px",
+                        marginTop: "15px",
+                      }}
+                    >
+                      Federica Ramirez
+                    </h5>
+                    <p className="card-text">
+                      Full Stack developer <br />
+                      Melo, Cerro Largo
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        <img
+                          src={lk}
+                          className="card-img-top rounded-circle float-end"
+                          style={{ margin: "10px", width: "12%" }}
+                          onClick={() =>
+                            (window.location.href =
+                              "https://www.linkedin.com/in/federica-ram%C3%ADrez-peluffo-b23bb0197/")
+                          }
+                        />
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div className="row">
           {/* Juan */}
           <div className="col" style={{ margin: "10px" }}>
-            <div className="card" style={{ width: "18rem" }}>
-              <img
-                src={juan}
-                className="card-img-top rounded-circle"
-                style={{ margin: "5px", width: "97%" }}
-              />
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <h2
-                    style={{
-                      color: "blueviolet",
-                      "font-family": "Inspiration, cursive",
-                      "font-size": "48px",
-                      marginTop: "20px",
-                    }}
-                  >
-                    Juan Moreira
-                  </h2>
-                </li>
-                <li className="list-group-item">
-                  <p>Full stack developer</p>
-                  Cerro Largo
-                </li>
-                <li className="list-group-item">
+            <div className="card mb-3" style={{ width: "540px" }}>
+              <div className="row g-0">
+                <div className="col-md-4">
                   <img
-                    src={lk}
-                    className="card-img-top rounded-circle float-end"
-                    style={{ margin: "5px", width: "20%" }}
-                    // onClick={window.location.replace(
-                    //   "https://www.linkedin.com/in/cecilia-perdomo/"
-                    // )}
+                    src={juan}
+                    className="img-fluid rounded-circle"
+                    style={{ margin: "10px", width: "97%", marginTop: "20px" }}
                   />
-                </li>
-              </ul>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5
+                      className="card-title"
+                      style={{
+                        color: "blueviolet",
+                        "font-family": "Inspiration, cursive",
+                        "font-size": "48px",
+                        marginTop: "15px",
+                      }}
+                    >
+                      Juan Moreira
+                    </h5>
+                    <p className="card-text">
+                      Full Stack developer <br />
+                      Melo, Cerro Largo
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        <img
+                          src={lk}
+                          className="card-img-top rounded-circle float-end"
+                          style={{ margin: "10px", width: "12%" }}
+                          onClick={() =>
+                            (window.location.href =
+                              "https://www.linkedin.com/in/juan-moreira-a0813b257/")
+                          }
+                        />
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           {/* Norbis */}
           <div className="col" style={{ margin: "10px" }}>
-            <div className="card" style={{ width: "18rem" }}>
-              <img
-                src={norbis}
-                className="card-img-top rounded-circle"
-                style={{ margin: "5px", width: "97%" }}
-              />
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <h2
-                    style={{
-                      color: "blueviolet",
-                      "font-family": "Inspiration, cursive",
-                      "font-size": "48px",
-                      marginTop: "20px",
-                    }}
-                  >
-                    Norbis Silveira
-                  </h2>
-                </li>
-                <li className="list-group-item">
-                  <p>Full stack developer</p>
-                  Vergara
-                </li>
-                <li className="list-group-item">
+            <div className="card mb-3" style={{ width: "540px" }}>
+              <div className="row g-0">
+                <div className="col-md-4">
                   <img
-                    src={lk}
-                    className="card-img-top rounded-circle float-end"
-                    style={{ margin: "5px", width: "20%" }}
-                    // onClick={window.location.replace(
-                    //   "https://www.linkedin.com/in/cecilia-perdomo/"
-                    // )}
+                    src={norbis}
+                    className="img-fluid rounded-circle"
+                    style={{ margin: "10px", width: "97%", marginTop: "20px" }}
                   />
-                </li>
-              </ul>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5
+                      className="card-title"
+                      style={{
+                        color: "blueviolet",
+                        "font-family": "Inspiration, cursive",
+                        "font-size": "48px",
+                        marginTop: "15px",
+                      }}
+                    >
+                      Norbis Silveira
+                    </h5>
+                    <p className="card-text">
+                      Full Stack developer <br />
+                      Vergara, Treinta y Tres
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        {/* <img
+                          src={lk}
+                          className="card-img-top rounded-circle float-end"
+                          style={{ margin: "10px", width: "12%" }}
+                          onClick={() =>
+                            (window.location.href =
+                              "https://www.linkedin.com/in/juan-moreira-a0813b257/")
+                          }
+                        /> */}
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
