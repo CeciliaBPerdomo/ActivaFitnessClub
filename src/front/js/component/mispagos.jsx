84d7e37ce8f78ee446f376decd0e33e36cdb2280
federicaramirez <federicaaramirez@gmail.com>
2022-11-23 15:20:41 +0000

    cambios en mis pagos

--- src/front/js/component/mispagos.jsx
+++ src/front/js/component/mispagos.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useContext, useEffect} from "react";
-import {Link, useParams} from "react-router-dom";
-import {Context} from "../store/appContext";
+import React, { useState, useContext, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
 import titulo from "../../img/Cabeceras/pagos.jpg";
 import Table from "react-bootstrap/Table";
 
@@ -9,100 +9,100 @@ import Table from "react-bootstrap/Table";
 // /////////////////////////////////////////
 
 export const MisPagos = () => {
-    const {store, actions} = useContext(Context);
-    const params = useParams();
+  const { store, actions } = useContext(Context);
+  const params = useParams();
 
-    useEffect(() => {
-        actions.obtenerMensualidadId(parseInt(params.theid));
+  useEffect(() => {
+    actions.obtenerMensualidadId(parseInt(params.theid));
 
-        const getInfo = async () => {
-            let {cuota} = await actions.obtenerAlumnoId(parseInt(params.theid));
-            // Mercado pago
-            actions.pagoMercadoPago(cuota);
-        };
-        getInfo();
-    }, []);
-
-    const pagoMercadoPago = () => {
-        window.location.replace(store ?. mercadopago.sandbox_init_point);
+    const getInfo = async () => {
+      let { cuota } = await actions.obtenerAlumnoId(parseInt(params.theid));
+      // Mercado pago
+      actions.pagoMercadoPago(cuota);
     };
+    getInfo();
+  }, []);
 
-    return (
-        <>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+  const pagoMercadoPago = () => {
+    window.location.replace(store?.mercadopago.sandbox_init_point);
+  };
 
-            <button type="button" className="btn btn-outline-danger justify-content-md-end float-end relative"
-                style={
-                    {
-                        marginRight: "125px",
-
-                        marginBottom: "20px",
-                        color: "white"
-                    }
-                }
-                onClick={pagoMercadoPago}>
-                Pagar mensualidad on-line
-            </button>
-            {/* </Link> */}
-            <div className="container">
-                <div>
-                    <br/>{" "}
-                    {/* <img src={titulo} style={{ width: "75%", height: "150px" }} /> */}
-                    <h1 style={
-                        {color: "white"}
-                    }>Mis pagos</h1>
-                    <br/>
-                </div>
-
-                {/* Listado de pagos */}
-                <pre className="d-flex mx-3 chroma">
-        <Table
-          striped
-          bordered
-          hover
-          variant="dark"
-          className="table table-hover table-secondary"
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="container vh-100">
+        <button
+          className="btn btn-danger float-end"
+          type="submit"
+          style={{
+            background: "#A00404",
+            marginBottom: "20px",
+            color: "white",
+          }}
+          onClick={pagoMercadoPago}
         >
-          <thead>
-            <tr>
-              {" "}
-              {/* Cabezeras */}
-              <th scope="col" className="text-center">
-                Fecha de ingreso
-              </th>
-              <th scope="col" className="text-center">
-                Fecha de pago
-              </th>
-              <th scope="col" className="text-center">
-                Monto
-              </th>
-              <th scope="col" className="text-center">
-                Factura
-              </th>
-              <th scope="col">Descripción</th>
-            </tr>
-          </thead>
+          Pagar mensualidad on-line
+        </button>
+        {/* </Link> */}
+        <div className="container">
+          <div>
+            <br />{" "}
+            {/* <img src={titulo} style={{ width: "75%", height: "150px" }} /> */}
+            <h1 style={{ color: "white" }}>Mis pagos</h1>
+            <br />
+          </div>
 
-          <tbody>
-            {" "}
-            {store.mens.map((item, id) => (
-              <tr key={id}>
-                <td className="text-center">
-                  {store.alumno.date_of_admission}{" "}
-                </td>
-                <td className="text-center">{item.payment_date}</td>
-                <td className="text-center">$ {item.payment_amount}</td>
-                <td className="text-center">{item.bill_n}</td>
-                <td>{item.description}</td>
-              </tr>
-            ))}{" "}
-          </tbody>
-        </Table>
-        </pre>
-            </div>
-        </>
-    );
+          {/* Listado de pagos */}
+          <pre className="d-flex mx-3 chroma">
+            <table
+              striped
+              bordered
+              hover
+              variant="dark"
+              className="table"
+              style={{ color: "white" }}
+            >
+              <thead>
+                <tr>
+                  {" "}
+                  {/* Cabezeras */}
+                  <th scope="col" className="text-center">
+                    Fecha de ingreso
+                  </th>
+                  <th scope="col" className="text-center">
+                    Fecha de pago
+                  </th>
+                  <th scope="col" className="text-center">
+                    Monto
+                  </th>
+                  <th scope="col" className="text-center">
+                    Factura
+                  </th>
+                  <th scope="col">Descripción</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {" "}
+                {store.mens.map((item, id) => (
+                  <tr key={id}>
+                    <td className="text-center">
+                      {store.alumno.date_of_admission}{" "}
+                    </td>
+                    <td className="text-center">{item.payment_date}</td>
+                    <td className="text-center">$ {item.payment_amount}</td>
+                    <td className="text-center">{item.bill_n}</td>
+                    <td>{item.description}</td>
+                  </tr>
+                ))}{" "}
+              </tbody>
+            </table>
+          </pre>
+        </div>
+      </div>
+    </>
+  );
 };
